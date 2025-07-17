6334a26f26f67b4483b6afa1cf6524e04393db87
Nícollas Frazão <nicollasfrazao@hotmail.com>
2025-07-17 20:00:07 -0300

    feat: create customers table

---
+++ database/migrations/2025_07_17_224022_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) 
+        {
+            $table->id();
+
+            $table->string('name', 150);
+            $table->string('cpf', 11)->unique();
+            $table->date('birth_date');
+            $table->decimal('income', 10, 2)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
